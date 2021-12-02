98f5f33bf9665dad89a60209507ec8f4ae43e6c5
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-12-02 13:22:07 +0900

    Add Logo component

--- src/components/Atoms/Logo.svelte
+++ src/components/Atoms/Logo.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 </script>
 
-<h1 class="font-sans font-black text-3xl">
-    LOGO
-</h1>
+<a href="#" class="flex items-center">
+    <svg width="48" height="58" viewBox="0 0 48 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13.409 33V28.8108C9.60599 29.2297 2 28.2243 2 20.8514C2 13.4784 8.76088 12.473 12.1413 12.8919C11.5779 9.26126 13.3245 2 24.818 2C39.1848 2 33.6916 21.2703 27.7759 17.0811C26.7899 15.4054 26.5082 12.2216 33.2691 12.8919C36.3678 12.1937 43.1569 12.473 45.5232 19.1757C47.8895 25.8784 40.8751 28.3919 37.0721 28.8108L35.8044 33H13.409Z" stroke="black" stroke-width="2.5" stroke-linejoin="round"/>
+        <path d="M17 19C17.8276 21.85 21.3862 27.64 29 28" stroke="black" stroke-linejoin="round"/>
+        <ellipse cx="20" cy="42" rx="2" ry="4" fill="black"/>
+        <ellipse cx="29" cy="42" rx="2" ry="4" fill="black"/>
+        <path d="M17 55.5517C18.9798 56.931 24.5515 58.5516 31 54" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M35.0074 50C33.5186 50.153 30.6402 51.1935 31.0372 54.1314C31.4342 57.0693 34.5112 57.1917 36 56.8856" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    <h1 class="font-sans font-bold pt-2 ml-2 text-3xl">
+        Golabab
+    </h1>
+</a>
 
 <style lang="scss">
 </style>
