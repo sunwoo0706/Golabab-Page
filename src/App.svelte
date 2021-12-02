9e88df46410eea0296e492d1d2b97b38de7186f1
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-12-02 12:24:22 +0900

    Implement RankCardWrapper

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import Header from "./components/Templates/Header.svelte";
   import Tailwind from "./Tailwind.svelte";
+  import RankCardWrapper from "./components/Templates/RankCardWrapper.svelte";
 </script>
 
 <Tailwind />
 <Header />
+<RankCardWrapper />
 
 <style lang="scss">
 </style>
