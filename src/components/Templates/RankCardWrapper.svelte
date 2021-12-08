d5da2b2119d230fd6008fd56f446e6d033156684
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-12-09 08:56:43 +0900

    Fix reponsibility

--- src/components/Templates/RankCardWrapper.svelte
+++ src/components/Templates/RankCardWrapper.svelte
@@ -3,16 +3,13 @@
   import Paragraph from "../Blocks/Paragraph.svelte";
 </script>
 
-<main class="absolute w-3/4 flex justify-around items-center">
+<section class="w-3/4 h-screen flex justify-around items-center">
     <RankCard theme="blue" title="2주간 급식 순위" />
     <Paragraph />
-</main>
+</section>
 
 <style lang="scss">
-    main {
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        height: fit-content;
+    section {
+        margin: 0 auto;
     }
 </style>
